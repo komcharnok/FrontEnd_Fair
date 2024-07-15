3175d8a9d7eef29a3b8264b3fadc5c440949aee7
MixDev <chaiwiwatmixx@gmail.com>
2024-07-15 17:31:11 +0700

    config route2

--- src/routes/Approuter.jsx
+++ src/routes/Approuter.jsx
@@ -1,52 +1,56 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { createBrowserRouter } from 'react-router-dom'
-import LoginForm from '../pages/LoginForm'
-import RegisterForm from '../pages/RegisterForm'
-import { RouterProvider } from 'react-router-dom'
-import NavBar from './../components/home/NavBar';
-import useAuth from '../hooks/useAuth'
-import HomePage from './../pages/home/HomePage';
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import LoginForm from "../pages/LoginForm";
+import RegisterForm from "../pages/RegisterForm";
+import { RouterProvider } from "react-router-dom";
+import NavBar from "./../components/home/NavBar";
+import useAuth from "../hooks/useAuth";
+import HomePage from "./../pages/home/HomePage";
+import searchProduct from "../pages/searchProduct/SearchProduct";
+import MainChat from "../pages/chatApp/MainChat";
 
 const userRouter = createBrowserRouter([
-    {
-        path: '/',
-        element: <>
-            <Outlet />
-        </>,
-        children: [
-            {index: true, element: <HomePage/>},
-            { path: "*", element: <h1>Page not found</h1> }
-        ]
-    }
-
-])
+  {
+    path: "/",
+    element: (
+      <>
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/chat", element: <MainChat /> },
+      { path: "*", element: <h1>Page not found</h1> },
+    ],
+  },
+]);
 
 const guestRouter = createBrowserRouter([
-    {
-        path: '/',
-        element: <>
-            <NavBar />
-            <Outlet />
-        </>,
-        children: [
-            {index: true, element:<LoginForm/>},
-            {path: 'register', element: <RegisterForm/>},
-            { path: "*", element: <h1>Page not found</h1> }
-        ]
-    }
-]
-)
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      { index: true, element: <LoginForm /> },
+      { path: "register", element: <RegisterForm /> },
+      { path: "search/:keyword", element: <searchProduct /> },
+      { path: "search/category/:category", element: <searchProduct /> },
+      { path: "*", element: <h1>Page not found</h1> },
+    ],
+  },
+]);
 
 const finalRouter = (user) => {
-    if(!user) return guestRouter
-    if(user) return userRouter
-  }
+  if (!user) return guestRouter;
+  if (user) return userRouter;
+};
 
-
-  export default function AppRouter() {
-    const {user} = useAuth()
-    return <RouterProvider router={finalRouter(user)} />;
-  }
-  
+export default function AppRouter() {
+  const { user } = useAuth();
+  return <RouterProvider router={finalRouter(user)} />;
+}
