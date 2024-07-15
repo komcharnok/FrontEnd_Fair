1a9a9fdf80c57b43bc20556954d5b671bfb15906
Komcharn <93968252+komcharnok@users.noreply.github.com>
2024-07-15 18:13:12 +0700

    Edit AppRouter2

--- src/routes/Approuter.jsx
+++ src/routes/Approuter.jsx
@@ -4,18 +4,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginForm from '../pages/LoginForm'
 import RegisterForm from '../pages/RegisterForm'
 import { RouterProvider } from 'react-router-dom'
-import NavBar from '../components/home/NavBar';
+import NavBar from './../components/home/NavBar';
 import useAuth from '../hooks/useAuth'
-import HomePage from '../pages/home/HomePage';
-
-//By.QQQ
-import ShopCard from '../pages/ShopCard/ShopCard';
-import Address from '../pages/Address/Address';
-import Pays from '../pages/Pay/Pays';
-import Order from '../pages/Order/Order';
-import OrderAll from '../components/Order/OrderAll'
-import PaySucess from '../components/Pay/PaySucess';
-import Nopay from '../components/Order/Nopay';
+import HomePage from './../pages/home/HomePage';
 
 
 const userRouter = createBrowserRouter([
@@ -25,18 +16,8 @@ const userRouter = createBrowserRouter([
             <Outlet />
         </>,
         children: [
-            { index: true, element: <HomePage /> },
-            { path: "*", element: <h1>Page not found</h1> },
-            { path: 'shopcart', element: <ShopCard /> },
-            { path: 'address', element: <Address /> },
-            { path: 'pays', element: <Pays /> },
-            { path: 'order', element: <Order /> },
-            { path: 'orderall', element: <OrderAll /> },
-            { path: 'paysucess', element: <PaySucess /> },
-            { path: 'nopay', element: <Nopay /> },
-            { path: 'revieworder', element: <Nopay /> },  
-
-
+            {index: true, element: <HomePage/>},
+            { path: "*", element: <h1>Page not found</h1> }
         ]
     }
 
@@ -50,11 +31,8 @@ const guestRouter = createBrowserRouter([
             <Outlet />
         </>,
         children: [
-            { index: true, element: <LoginForm /> },
-            { path: 'register', element: <RegisterForm /> },
-
-            
-              
+            {index: true, element:<LoginForm/>},
+            {path: 'register', element: <RegisterForm/>},
             { path: "*", element: <h1>Page not found</h1> }
         ]
     }
@@ -62,13 +40,13 @@ const guestRouter = createBrowserRouter([
 )
 
 const finalRouter = (user) => {
-    if (!user) return guestRouter
-    if (user) return userRouter
-}
+    if(!user) return guestRouter
+    if(user) return userRouter
+  }
 
 
-export default function AppRouter() {
-    const { user } = useAuth()
+  export default function AppRouter() {
+    const {user} = useAuth()
     return <RouterProvider router={finalRouter(user)} />;
-}
-
+  }
+  
