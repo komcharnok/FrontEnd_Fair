84d3c400db36a9accac54a43cfee9df0134fd828
MixDev <chaiwiwatmixx@gmail.com>
2024-07-15 18:00:32 +0700

    config route9

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 import useAuth from "./hooks/useAuth";
-<<<<<<< HEAD
-=======
-
->>>>>>> e32c6fe1966da936e000c0485524f37d6064dd79
 import SearchProduct from "./pages/searchProduct/SearchProduct";
 import TodoList from "./pages/todo/TodoList";
 import { useUser } from "./store/store";
