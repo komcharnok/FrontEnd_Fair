b45015f89f6048c93f0ce660c7e9a011032133e7
MixDev <chaiwiwatmixx@gmail.com>
2024-07-17 00:20:20 +0700

    chat

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@
 import useAuth from "./hooks/useAuth";
 import AppRouter from "./routes/AppRoute";
 
-
 function App() {
   const { loading } = useAuth();
   return (
-    <div className="min-h-screen flex flex-col gap-5 items-center justify-start font-Prompt" data-theme="light">
+    <div className="container mx-auto font-Prompt" data-theme="light">
       {loading && <span className="loading loading-spinner loading-md"></span>}
       {!loading && <AppRouter />}
     </div>
